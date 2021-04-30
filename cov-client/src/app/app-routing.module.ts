d18bba337e5ed4ca49ffc955d513e20d62c77fcd
zer0leet <diwakar29buddy@gmail.com>
2021-04-30 12:57:52 +0545

    "BASIC CONFI"

--- cov-client/src/app/app-routing.module.ts
+++ cov-client/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'dashboard-component', component: DashboardComponent },
+  { path: 'header-component', component: HeaderComponent },
+  { path: 'footer-component', component: FooterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+ }
--- cov-client/src/app/app.module.ts
+++ cov-client/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
