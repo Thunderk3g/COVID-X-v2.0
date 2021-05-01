0a537cff3a26f95a981f3acd2b3cb00c021d709b
zer0leet <diwakar29buddy@gmail.com>
2021-05-01 11:00:17 +0545

    Minor Changes

---
+++ cov-client/src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WindowService } from '../common/window/window.service';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    WindowService
+  ]
+})
+export class HomeModule { }
