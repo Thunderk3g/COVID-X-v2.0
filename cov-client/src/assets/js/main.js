9f30a7f0ee73583638e2cff32fdadc68bbfecb7f
Abhishek Pandey <33335446+EnRyuuX7@users.noreply.github.com>
2021-05-01 10:56:03 +0545

    Start with semantic UI general layout

---
+++ cov-client/src/assets/js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('.dropdown.item').on('click', function(){
+    $('inverted.menu').addClass('drop');
+  });
+  $('.plus')
+    .on('click', function() {
+      $('.search.item, .primary > .item:not(:last-of-type)')
+        .hide();
+      $('.slide > .item')
+        .addClass('open');
+      $('.slide .remove')
+        .addClass('spin');
+  });
+  $('.remove')
+    .on('click', function() {
+    $(this)
+      .removeClass('spin');
+    $('.search.item, .primary > .item:not(:last-of-type)')
+      .show();
+    $('.slide > .item')
+      .removeClass('open');
+  });
+});
