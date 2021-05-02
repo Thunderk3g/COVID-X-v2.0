7dfecc985135ebf6fa44dc76527252ca17fbfb94
zer0leet <diwakar29buddy@gmail.com>
2021-05-02 16:41:34 +0545

    server-stuff

--- cov-server/app.js
+++ cov-server/app.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
+app.listen(1234,() => console.log("Application is listening at PORT NUMBER 1234"))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
