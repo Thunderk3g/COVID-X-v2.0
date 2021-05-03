b039abec97c3481fef7991f6eebdaf2c218995b8
zer0leet <diwakar29buddy@gmail.com>
2021-05-04 00:34:23 +0545

    css changes minor!

--- cov-client/src/app/home/home.component.ts
+++ cov-client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { AppHttpService } from '../common/app-http.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,11 +9,19 @@ import {HttpClient} from '@angular/common/http';
 export class HomeComponent implements OnInit {
 
   li:any;
-  constructor(private http : HttpClient){
+  data:any;
+  constructor(private http : HttpClient,
+            private httpservice : AppHttpService,
+    ){
+
     this.li={};
 }
 
   ngOnInit(): void {
+    this.httpservice.entries().subscribe((data) => {
+      this.data = data.body;
+      console.log(data);
+    });
     this.http.get('https://corona.askbhunte.com/api/v1/data/nepal')
     .subscribe((response) => {
       console.log(response)
