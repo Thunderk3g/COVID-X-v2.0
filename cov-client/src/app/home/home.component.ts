88355293865f4cd577b21848ec1b1b940baf03fc
Abhishek Pandey <33335446+EnRyuuX7@users.noreply.github.com>
2021-05-05 14:19:49 +0545

    Css edited

--- cov-client/src/app/home/home.component.ts
+++ cov-client/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { AppHttpService } from '../common/app-http.service';
 @Component({
   selector: 'app-home',
@@ -7,20 +7,20 @@ import { AppHttpService } from '../common/app-http.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  district:any;
-  li:any;
-  list:any;
-  data:any;
-  dist:any;
-  public show:boolean = false;
-  public buttonName:any = 'Show';
-  constructor(private http : HttpClient,
-            private httpservice : AppHttpService,
-    ){
-    this.li={};
-    this.dist= new Object();
-    this.district="1";
-}
+  district: any;
+  li: any;
+  list: any;
+  data: any;
+  dist: any;
+  public show: boolean = false;
+  public buttonName: any = 'Show';
+  constructor(private http: HttpClient,
+    private httpservice: AppHttpService,
+  ) {
+    this.li = {};
+    this.dist = new Object();
+    this.district = "1";
+  }
 
   ngOnInit(): void {
     this.httpservice.entries().subscribe((data) => {
@@ -28,34 +28,34 @@ export class HomeComponent implements OnInit {
       this.filter('bed');
     });
     this.http.get('https://corona.askbhunte.com/api/v1/data/nepal')
-    .subscribe((response) => {
-      this.li=response;
-    })
+      .subscribe((response) => {
+        this.li = response;
+      })
     this.http.get('https://data.askbhunte.com/api/v1/districts')
-    .subscribe((response) => {
-      this.dist=response;
-    })
-    this.http.get('https://data.askbhunte.com/api/v1/districts/'+this.district)
+      .subscribe((response) => {
+        this.dist = response;
+      })
+    this.http.get('https://data.askbhunte.com/api/v1/districts/' + this.district)
       .subscribe((response) => {
         this.dist.title = response['title'];
         this.dist.title_ne = response['title_ne'];
         this.dist.covid_summary = response['covid_summary'];
         console.log(response);
       })
-}
-filter(type:string){
-  this.list =[];
+  }
+  filter(type: string) {
+    this.list = [];
     this.data.forEach(element => {
-      if(element.requestfor === type){
+      if (element.requestfor === type) {
         this.list.push(element);
       }
     });
-  if(this.list.length==0){
-    this.list.push({firstname:"N/A" , lastname:"" , requestfor:"N/A" , urgency:"N/A" , address:"N/A"});
+    if (this.list.length == 0) {
+      this.list.push({ firstname: "N/A", lastname: "", requestfor: "N/A", urgency: "N/A", address: "N/A" });
+    }
   }
-  }
-  loadDatabyDistrict(){
-      this.http.get('https://data.askbhunte.com/api/v1/districts/'+this.district)
+  loadDatabyDistrict() {
+    this.http.get('https://data.askbhunte.com/api/v1/districts/' + this.district)
       .subscribe((response) => {
         this.dist.title = response['title'];
         this.dist.title_ne = response['title_ne'];
@@ -68,12 +68,12 @@ filter(type:string){
 
     // CHANGE THE NAME OF THE BUTTON.
 
-    if(this.show)
+    if (this.show)
       this.buttonName = "Hide";
     else
       this.buttonName = "Show";
   }
-  toggleclose(){
+  toggleclose() {
     this.show = !this.show;
     this.buttonName = "Hide";
   }
