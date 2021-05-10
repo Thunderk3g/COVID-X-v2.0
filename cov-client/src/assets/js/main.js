25c04466763eea312477e60d249cc8b44a67c959
Abhishek Pandey <33335446+EnRyuuX7@users.noreply.github.com>
2021-05-10 12:29:51 +0545

    Validation

--- cov-client/src/assets/js/main.js
+++ cov-client/src/assets/js/main.js
@@ -58,135 +58,239 @@ $(function () {
   //   });
   // };
 
-  $('.needhelp').form({
-    fields: {
-      firstname: {
-        identifier: 'firstname',
-        rules: [{
-          type: 'empty',
-          prompt: 'Please enter your First name'
-        }]
-      },
-      lastname: {
-        identifier: 'lastname',
-        rules: [{
-          type: 'empty',
-          prompt: 'Please enter your Last name'
-        }]
-      },
-      address: {
-        identifier: 'address',
-        rules: [{
-          type: 'minLength[3]',
-          prompt: 'Please enter a valid address.'
-        }]
-      },
-      urgency: {
-        identifier: 'urgency',
-        rules: [{
-          type: 'empty',
-          prompt: 'Please specify your urgency.'
-        }]
-      },
-      gender: {
-        identifier: 'gender',
-        rules: [{
-          type: 'empty',
-          prompt: 'Please select a gender'
-        }]
-      },
-      email: {
-        identifier: 'email',
-        rules: [{
-          type: 'email',
-          prompt: 'Please enter a vaid Email address.'
-        }]
-      },
-      requestfor: {
-        identifier: 'requestfor',
-        rules: [{
-          type: 'empty',
-          prompt: 'Please select a request'
-        }]
-      },
-      ph_no: {
-        identifier: 'ph_no',
-        rules: [{
-            type: 'empty',
-            prompt: 'Please enter a password'
-          },
-          {
-            type: 'minLength[10]',
-            prompt: 'Please enter a a valid 10-digit Ph-number'
-          }
-        ]
-      }
-    }
+  //Validate Full name
+  $('#namecheck').hide();
+  let nameError = true;
+  $('#fullname').keyup(function () {
+    validateName();
   });
 
-
-  $('.contribute').form({
-    fields: {
-      fullname: {
-        identifier: 'fullname',
-        rules: [{
-          type: 'empty',
-          prompt: 'Please enter your Full name'
-        }]
-      },
-      address: {
-        identifier: 'address',
-        rules: [{
-          type: 'minLength[3]',
-          prompt: 'Please enter a valid address.'
-        }]
-      },
-      email: {
-        identifier: 'email',
-        rules: [{
-          type: 'email',
-          prompt: 'Please enter a vaid Email address.'
-        }]
-      },
-      ph_no: {
-        identifier: 'contact',
-        rules: [{
-            type: 'empty',
-            prompt: 'Please enter your contact number.'
-          },
-          {
-            type: 'minLength[10]',
-            prompt: 'Please enter a a valid 10-digit mobile number'
-          }
-        ]
-      },
-      volunteerfor: {
-        identifier: 'volunteerfor',
-        rules: [{
-          type: 'empty',
-          prompt: 'Please select what you want to volunteer for.'
-        }]
-      },
+  function validateName() {
+    let nameValue = $('#fullname').val();
+    if (nameValue.length == '') {
+      $('#namecheck').show();
+      nameError = false;
+      return false;
+    } else if ((nameValue.length < 3) ||
+      (nameValue.length > 10)) {
+      $('#namecheck').show();
+      $('#namecheck').html("**length of username must be between 3 and 10");
+      nameError = false;
+      return false;
+    } else {
+      $('#namecheck').hide();
     }
+  }
+  //Address
+  $('#addresscheck').hide();
+  let addError = true;
+  $('#address').keyup(function () {
+    validateAddress();
   });
 
-  $('.contribute').form({
-    fields: {
-      username: {
-        identifier: 'username',
-        rules: [{
-          type: 'empty',
-          prompt: 'Username is required'
-        }]
-      },
-      address: {
-        identifier: 'password',
-        rules: [{
-          type: 'empty',
-          prompt: 'Password is required'
-        }]
-      },
+  function validateAddress() {
+    let addressValue = $('#address').val();
+    if (addressValue.length == '') {
+      $('#addresscheck').show();
+      addError = false;
+      return false;
+    } else {
+      $('#addresscheck').hide();
     }
+  }
+
+
+  // Validate Contact
+  $('#contactcheck').hide();
+  let contactError = true;
+  $('#contact').keyup(function () {
+    validateContact();
   });
 
+  function validateContact() {
+    let contactValue = $('#contact').val();
+    if (contactValue.length == '') {
+      $('#contactcheck').show();
+      contactError = false;
+      return false;
+    } else if (contactValue.length != 10) {
+      $('#contactcheck').show();
+      $('#contactcheck').html("**Enter a valid 10 digit contact number");
+      contactError = false;
+      return false;
+    } else {
+      $('#contactcheck').hide();
+    }
+  }
+
+  // Validate selection
+  $('#volunteererr').hide();
+  let volunteerError = true;
+  $('#volunteerFor').keyup(function () {
+    validatevolunteer();
+  });
+
+  function validatevolunteer() {
+    let volValue = $('#volunteerFor').val();
+    if (volValue.length == '') {
+      $('#volunteererr').show();
+      volunteerError = false;
+      return false;
+    } else {
+      $('#volunteererr').hide();
+    }
+  }
+
+  // Validate Email
+  const email = $('#email').val();
+  email.addEventListener('blur', () => {
+    let regex =
+      /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+    let s = email.value;
+    if (regex.test(s)) {
+      email.classList.remove(
+        'is-invalid');
+      emailError = true;
+    } else {
+      email.classList.add(
+        'is-invalid');
+      emailError = false;
+    }
+  })
 });
+
+
+
+
+///dump
+
+// $('.needhelp').form({
+//   fields: {
+//     firstname: {
+//       identifier: 'firstname',
+//       rules: [{
+//         type: 'empty',
+//         prompt: 'Please enter your First name'
+//       }]
+//     },
+//     lastname: {
+//       identifier: 'lastname',
+//       rules: [{
+//         type: 'empty',
+//         prompt: 'Please enter your Last name'
+//       }]
+//     },
+//     address: {
+//       identifier: 'address',
+//       rules: [{
+//         type: 'minLength[3]',
+//         prompt: 'Please enter a valid address.'
+//       }]
+//     },
+//     urgency: {
+//       identifier: 'urgency',
+//       rules: [{
+//         type: 'empty',
+//         prompt: 'Please specify your urgency.'
+//       }]
+//     },
+//     gender: {
+//       identifier: 'gender',
+//       rules: [{
+//         type: 'empty',
+//         prompt: 'Please select a gender'
+//       }]
+//     },
+//     email: {
+//       identifier: 'email',
+//       rules: [{
+//         type: 'email',
+//         prompt: 'Please enter a vaid Email address.'
+//       }]
+//     },
+//     requestfor: {
+//       identifier: 'requestfor',
+//       rules: [{
+//         type: 'empty',
+//         prompt: 'Please select a request'
+//       }]
+//     },
+//     ph_no: {
+//       identifier: 'ph_no',
+//       rules: [{
+//           type: 'empty',
+//           prompt: 'Please enter a password'
+//         },
+//         {
+//           type: 'minLength[10]',
+//           prompt: 'Please enter a a valid 10-digit Ph-number'
+//         }
+//       ]
+//     }
+//   }
+// });
+
+
+// $('.contribute').form({
+//   fields: {
+//     fullname: {
+//       identifier: 'fullname',
+//       rules: [{
+//         type: 'empty',
+//         prompt: 'Please enter your Full name'
+//       }]
+//     },
+//     address: {
+//       identifier: 'address',
+//       rules: [{
+//         type: 'minLength[3]',
+//         prompt: 'Please enter a valid address.'
+//       }]
+//     },
+//     email: {
+//       identifier: 'email',
+//       rules: [{
+//         type: 'email',
+//         prompt: 'Please enter a vaid Email address.'
+//       }]
+//     },
+//     ph_no: {
+//       identifier: 'contact',
+//       rules: [{
+//           type: 'empty',
+//           prompt: 'Please enter your contact number.'
+//         },
+//         {
+//           type: 'minLength[10]',
+//           prompt: 'Please enter a a valid 10-digit mobile number'
+//         }
+//       ]
+//     },
+//     volunteerfor: {
+//       identifier: 'volunteerfor',
+//       rules: [{
+//         type: 'empty',
+//         prompt: 'Please select what you want to volunteer for.'
+//       }]
+//     },
+//   }
+// });
+
+// $('.contribute').form({
+//   fields: {
+//     username: {
+//       identifier: 'username',
+//       rules: [{
+//         type: 'empty',
+//         prompt: 'Username is required'
+//       }]
+//     },
+//     address: {
+//       identifier: 'password',
+//       rules: [{
+//         type: 'empty',
+//         prompt: 'Password is required'
+//       }]
+//     },
+//   }
+// });
